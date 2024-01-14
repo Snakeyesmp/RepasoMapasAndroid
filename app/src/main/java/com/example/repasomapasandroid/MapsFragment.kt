baaacae7003c2d1e5b8d46cebe558041c440fd2c
Mario Muñoz <snakeyes271101@gmail.com>
2024-01-14 13:29:08 +0100

    Initial commit (Hace cosas al pulsar y mantener pulsado)

---
+++ app/src/main/java/com/example/repasomapasandroid/MapsFragment.kt
@@ -0,0 +1,69 @@
+package com.example.repasomapasandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : SupportMapFragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+
+        // Notifica cuando el mapa esté listo
+        getMapAsync(this)
+
+        return view
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Configuración del mapa
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
+        // OnMapClickListener
+        mMap.setOnMapClickListener { latLng ->
+            Toast.makeText(
+                requireContext(),
+                "Latitud: ${latLng.latitude}, Longitud: ${latLng.longitude}",
+                Toast.LENGTH_LONG
+            ).show()
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title("Marcador en destino")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            )
+        }
+
+        // OnMapLongClickListener
+        mMap.setOnMapLongClickListener { latLng ->
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title("Marcador largo")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                    .snippet("Teléfono: 983989784")
+            )
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        }
+    }
+}
